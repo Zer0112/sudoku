50bc8a35d5bf063e2420afa30c216b822353bc4d
Stefan Schaller <st.schaller11@gmail.com>
2020-07-17 12:31:54 +0200

    start

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
 module Lib
-    ( someFunc
-    ) where
+    ( printWelcome
+    )
+where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+printWelcome :: IO ()
+printWelcome = do
+    print "Welcome to sudoku the game"
+    print "Press 1 for playing the game"
+    print "Press 2 for solving a sudoku"
+    print "Press any other key to exit"
+    arg <- getChar
+    print arg

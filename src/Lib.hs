99653c9dbe0aece4681309a0bc849e34e3505402
Stefan Schaller <st.schaller11@gmail.com>
2020-07-17 12:43:05 +0200

    added simple cli functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,8 +3,14 @@ module Lib
     )
 where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+
+inputPhraser :: [Char] -> [Char]
+inputPhraser arg | arg == "1" = "option 1"
+                 | arg == "2" = "option 2"
+                 | arg == "3" = "option 4"
+                 | otherwise  = "exit"
+
 
 
 printWelcome :: IO ()
@@ -12,6 +18,8 @@ printWelcome = do
     print "Welcome to sudoku the game"
     print "Press 1 for playing the game"
     print "Press 2 for solving a sudoku"
+    print "Press 3 for help"
     print "Press any other key to exit"
-    arg <- getChar
-    print arg
+    arg <- getLine
+    print $ inputPhraser arg
+
